cb6819d3cc0eb1f66827b04f26708a212f612a5f
Pranav Paliwal <pranavpaliwal6@gmail.com>
2024-08-04 03:11:54 +0530

    Development Pahse Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:spendings/widgets/expenses.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+     MaterialApp(
+      theme: ThemeData().copyWith(useMaterial3: true,
+      scaffoldBackgroundColor:Color.fromARGB(255, 255, 243, 199)),
+      home: const Expenses(),
     ),
   );
 }
